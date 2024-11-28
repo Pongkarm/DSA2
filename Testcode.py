4d4028bc83f42376219abe3961e6dd89cf6c2ba3
pongkarm <darunpoppong@gmail.com>
2024-11-28 23:37:35 +0700

    add result_province

---
+++ Testcode.py
@@ -0,0 +1,18 @@
+import re
+
+#EX 1 
+
+txt = "The rain in Spain"
+x = re.search("^The.*Spain$", txt)
+
+if x:
+  print("YES! We have a match!")
+else:
+  print("No match")
+  
+
+#EX2
+
+txt = "The rain in Spain"
+x = re.split("\s", txt)
+print(x)
