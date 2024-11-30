b337dc67b9833db917c9a50b40af78e87f3e2eb9
pongkarm <darunpoppong@gmail.com>
2024-12-01 06:18:18 +0700

    Revert "add file teachcode"
    This reverts commit 58f5b85c11549a64c6fa93ef5a8a275a81e5ce12.

--- Testcode.py
+++ Testcode.py
@@ -19,24 +19,14 @@ import re
 
 import re
 
-# text = "My email is example@test.com and phone is 123-456-7890."
-
-# # ค้นหาอีเมลและเบอร์โทรศัพท์ในข้อความ
-# match = re.search(r"(\S+@\S+)\s.*?(\d{3}-\d{3}-\d{4})", text)
-
-# if match:
-#     email = match.group(1)  # กลุ่มแรกที่จับคืออีเมล
-#     phone = match.group(2)  # กลุ่มที่สองที่จับคือเบอร์โทรศัพท์
-#     print(f"Email: {email}")
-#     print(f"Phone: {phone}")
-
-
-
-text = "My email is example@test.com and phone is '123'-'456-7890'."
+text = "My email is example@test.com and phone is 123-456-7890."
 
 # ค้นหาอีเมลและเบอร์โทรศัพท์ในข้อความ
-match = re.findall(r"\d+'", text)
+match = re.search(r"(\S+@\S+)\s.*?(\d{3}-\d{3}-\d{4})", text)
 
 if match:
-    email = match
-    print(f"Email: {match}")
+    email = match.group(1)  # กลุ่มแรกที่จับคืออีเมล
+    phone = match.group(2)  # กลุ่มที่สองที่จับคือเบอร์โทรศัพท์
+    print(f"Email: {email}")
+    print(f"Phone: {phone}")
+
